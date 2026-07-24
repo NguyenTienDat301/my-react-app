19932015ce0f871aadd7d6216a58f2bf7f4bcbff
tiendatkhongdeptrai? <nguyentiendat.3010204@gmail.com>
2026-07-24 09:35:33 +0700

    not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-const NotFound: React.FC = () => {
-  return <h1>404 - Không tìm thấy trang</h1>;
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: 100,
+        fontSize: 30,
+      }}
+    >
+      404 - Không tìm thấy trang
+    </div>
+  );
 };
 
 export default NotFound;
